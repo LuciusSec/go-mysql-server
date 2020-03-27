ea85d2d6decbf047f9f7064b1cba0b19d61a407b
hartnett <x@xsec.io>
2020-03-27 12:32:25 +0800

    [#] fix error

--- sql/parse/parse.go
+++ sql/parse/parse.go
@@ -247,7 +247,7 @@ func convertShow(s *sqlparser.Show, query string) (sql.Node, error) {
 		}
 
 		return node, nil
-	case sqlparser.KeywordString(sqlparser.DATABASES), sqlparser.KeywordString(sqlparser.SCHEMAS):
+	case sqlparser.KeywordString(sqlparser.DATABASES):
 		return plan.NewShowDatabases(), nil
 	case sqlparser.KeywordString(sqlparser.FIELDS), sqlparser.KeywordString(sqlparser.COLUMNS):
 		// TODO(erizocosmico): vitess parser does not support EXTENDED.
